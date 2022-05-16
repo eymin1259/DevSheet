f3f6d3b129ca2d7ee8e19c94cf35a7828d0dbc1d
eymin1259 <eymin1259@gmail.com>
2022-05-16 21:47:59 +0900

    ADD : DI Container 정의

---
+++ DevSheet/DevSheet/Sources/App/DI/DIContainer.swift
@@ -0,0 +1,40 @@
+//
+//  DIContainer.swift
+//  DevSheet
+//
+//  Created by yongmin lee on 5/16/22.
+//
+
+import Foundation
+import Swinject
+
+extension Container {
+    
+    func registerDependencies(){
+        registerService()
+        registerRepository()
+        registerUseCase()
+        registerReactor()
+        registerViewController()
+    }
+    
+    private func registerService(){
+        
+    }
+    
+    private func registerRepository() {
+        
+    }
+    
+    private func registerUseCase() {
+       
+    }
+    
+    private func registerReactor(){
+       
+    }
+    
+    private func registerViewController(){
+        
+    }
+}

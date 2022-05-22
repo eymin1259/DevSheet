26110313a326d93b5237b3af82451065e9f12361
eymin1259 <eymin1259@gmail.com>
2022-05-22 16:01:07 +0900

    ADD : SplashViewController UI 구현

--- DevSheet/DevSheet/Sources/Presentation/Splash/SplashViewController.swift
+++ DevSheet/DevSheet/Sources/Presentation/Splash/SplashViewController.swift
@@ -15,8 +15,29 @@ class SplashViewController: UIViewController, View {
     // MARK: properties
     typealias Reactor = SplashReactor
     var disposeBag: DisposeBag = .init()
+    let FONT_BM = "BMHANNA11yrsold"
 
     // MARK: UI
+    let splashImageView: UIImageView = {
+       var iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.backgroundColor = .clear
+        let randomImgNum = Int.random(in: 1..<4)
+        if let randomImg = UIImage(named: "splash\(randomImgNum)") {
+            iv.image = randomImg
+        }
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+    
+    lazy var splashLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.font = UIFont(name: FONT_BM, size: 40)
+        lbl.text = "개발족보"
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        return lbl
+        
+    }()
     
     // MARK: initialize
     init(reactor: Reactor) {
@@ -31,12 +52,32 @@ class SplashViewController: UIViewController, View {
     // MARK: life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        setupUI()
     }
 
     // MARK: methods
+    func setupUI() {
+        // view
+        self.view.backgroundColor = .white
+        
+        // imageView
+        self.view.addSubview(splashImageView)
+        splashImageView.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(20)
+            $0.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(270)
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview().offset(-50)
+        }
+        
+        // splashLabel
+        self.view.addSubview(splashLabel)
+        splashLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(splashImageView.snp.bottom).offset(10)
+        }
+    }
 }
-
 
 // MARK: Reactor Bind
 extension SplashViewController {

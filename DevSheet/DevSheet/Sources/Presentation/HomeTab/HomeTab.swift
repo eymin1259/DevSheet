45ed5109c2da996214a55e27ad7d8008bf1fde0a
eymin1259 <eymin1259@gmail.com>
2022-05-22 16:09:21 +0900

    ADD : TabBar 관련 파일 생성

---
+++ DevSheet/DevSheet/Sources/Presentation/HomeTab/HomeTab.swift
@@ -0,0 +1,15 @@
+//
+//  HomeTab.swift
+//  DevSheet
+//
+//  Created by yongmin lee on 5/20/22.
+//
+
+import Foundation
+
+enum HomeTab: Int, CaseIterable {
+    case cs
+    case develop
+    case like
+    case mypage
+}
---
+++ DevSheet/DevSheet/Sources/Presentation/HomeTab/HomeTabViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeTabViewModel.swift
+//  DevSheet
+//
+//  Created by yongmin lee on 5/20/22.
+//
+
+import Foundation

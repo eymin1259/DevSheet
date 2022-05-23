d828726ca670b9de1ee989b26c64d2639a71bc91
eymin1259 <eymin1259@gmail.com>
2022-05-23 16:24:41 +0900

    ADD : PresentationLayer에 CategoryListVC, MyPageVC 파일 생성

---
+++ DevSheet/DevSheet/Sources/Presentation/CategoryList/CategoryListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryListViewController.swift
+//  DevSheet
+//
+//  Created by yongmin lee on 5/23/22.
+//
+
+import UIKit
+
+final class CategoryListViewController: UIViewController {
+    
+    // MARK: life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("debug :CategoryListViewController  viewDidLoad ")
+    }
+}

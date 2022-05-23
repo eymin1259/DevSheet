8c71d7fdabc270f4557ec757fce4c0564682bd30
eymin1259 <eymin1259@gmail.com>
2022-05-23 16:56:45 +0900

    ADD : Inject MainTabBarController, MainTabBarViewModel  Dependency

--- DevSheet/DevSheet/Sources/App/DI/DIContainer.swift
+++ DevSheet/DevSheet/Sources/App/DI/DIContainer.swift
@@ -15,12 +15,13 @@ extension Container {
         registerRepository()
         registerUseCase()
         registerReactor()
+        registerViewModel()
         registerViewController()
     }
     
     private func registerService() {
         register(FirebaseService.self) { _ in
-            FirebaseServiceImpl()
+            return FirebaseServiceImpl()
         }
     }
     
@@ -48,10 +49,49 @@ extension Container {
         }
     }
     
+    private func registerViewModel() {
+        register(MainTabViewModel.self) { _ in
+            return MainTabViewModel()
+        }
+    }
+    
     private func registerViewController() {
         register(SplashViewController.self) { r in
             let reactor = r.resolve(SplashReactor.self)!
             let vc = SplashViewController(reactor: reactor)
+            return vc
+        }
+        
+        register(CategoryListViewController.self) { _ in
+            let vc = CategoryListViewController()
+            return vc
+        }.inObjectScope(.transient)
+        
+        register(MypageViewController.self) { _ in
+            let vc = MypageViewController()
+            return vc
+        }
+        
+        register(MainTabBarController.self) { r in
+            let viewModel = r.resolve(MainTabViewModel.self)!
+            let vc =  MainTabBarController(
+                viewModel: viewModel,
+                viewControllerFactory: { tab in
+                    let createdVC: UIViewController
+                    switch tab {
+                    case .cs:
+                        createdVC = r.resolve(CategoryListViewController.self)!
+                    case .develop:
+                        createdVC = r.resolve(CategoryListViewController.self)!
+                    case .favorite:
+                        createdVC = r.resolve(CategoryListViewController.self)!
+                    case .mypage:
+                        createdVC = r.resolve(MypageViewController.self)!
+                    }
+                    createdVC.tabBarItem = tab.getTabBarItem()
+                    return createdVC
+                }
+            )
             return vc
         }
     }
--- DevSheet/DevSheet/Sources/Presentation/MainTab/MainTabBarController.swift
+++ DevSheet/DevSheet/Sources/Presentation/MainTab/MainTabBarController.swift
@@ -6,3 +6,66 @@
 //
 
 import Foundation
+import UIKit
+import RxSwift
+
+final class MainTabBarController: UITabBarController {
+    
+    // MARK: properties
+    private var disposeBag: DisposeBag = .init()
+    private let viewModel: MainTabViewModel
+    private let viewControllerFactory: (MainTab) -> UIViewController
+    
+    // MARK: initialize
+    init(
+        viewModel: MainTabViewModel,
+        viewControllerFactory: @escaping (MainTab) -> UIViewController
+    ) {
+        self.viewModel = viewModel
+        self.viewControllerFactory = viewControllerFactory
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        beaverLog.debug("MainTabBarController  viewDidLoad")
+        
+        setup()
+        bind()
+    }
+    
+    // MARK: methods
+    func setup() {
+        view.backgroundColor = .white
+        tabBar.tintColor = .black
+        tabBar.backgroundColor = .systemGray6
+        tabBar.layer.shadowColor = UIColor.black.cgColor
+        tabBar.layer.shadowOpacity = 0.25
+        tabBar.layer.shadowOffset = CGSize(width: 0, height: 0)
+        tabBar.layer.shadowRadius = 2.0
+        
+    }
+}
+
+extension MainTabBarController {
+    
+    func bind() {
+        viewModel.tabItems
+            .distinctUntilChanged()
+            .subscribe( onNext: { [weak self] tabItems in
+                guard let self = self else { return }
+                let viewControllers = tabItems.map { tabItem -> UIViewController in
+                    let viewController = self.viewControllerFactory(tabItem)
+                    return viewController
+                }
+                beaverLog.debug("MainTabBarController  setViewControllers", context: tabItems)
+                self.setViewControllers(viewControllers, animated: true)
+            })
+            .disposed(by: disposeBag)
+    }
+}

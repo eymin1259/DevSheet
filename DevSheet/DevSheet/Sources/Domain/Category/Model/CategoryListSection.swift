50f5b23bad712da8276fde2b0320db5bc5c184c7
eymin1259 <eymin1259@gmail.com>
2022-05-24 22:50:28 +0900

    ADD : CategoryListSection Model 정의

---
+++ DevSheet/DevSheet/Sources/Domain/Category/Model/CategoryListSection.swift
@@ -0,0 +1,26 @@
+//
+//  CategoryListSection.swift
+//  DevSheet
+//
+//  Created by yongmin lee on 5/24/22.
+//
+
+import Foundation
+import RxDataSources
+
+
+struct CategoryListSection {
+    var categoryList: [Category]
+}
+
+extension CategoryListSection : SectionModelType {
+    
+    var items: [Category] {
+        return self.categoryList
+    }
+    
+    init(original: CategoryListSection, items: [Category]) {
+        self = original
+        self.categoryList = items
+    }
+}

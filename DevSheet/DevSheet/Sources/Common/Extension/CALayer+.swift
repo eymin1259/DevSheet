b8b13b8560bc1284b5f27baa799ae2c00dfa0dbc
eymin1259 <eymin1259@gmail.com>
2022-05-24 21:30:32 +0900

    ADD : layer에 그림자 만드는 함수 구현

---
+++ DevSheet/DevSheet/Sources/Common/Extension/CALayer+.swift
@@ -0,0 +1,21 @@
+//
+//  CALayer+.swift
+//  DevSheet
+//
+//  Created by yongmin lee on 5/24/22.
+//
+
+import UIKit
+
+extension CALayer {
+    func makeShadow(
+        color: CGColor = UIColor.black.cgColor,
+        alpha: Float = 0.25,
+        offset: CGSize = .zero,
+        blur: CGFloat = 1.0) {
+        self.shadowColor = color
+        self.shadowOpacity = alpha
+        self.shadowOffset = offset
+        self.shadowRadius = blur
+    }
+}

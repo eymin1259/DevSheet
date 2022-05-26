19360116319d8ce42509ad1903901f96b9de8e55
eymin1259 <eymin1259@gmail.com>
2022-05-26 21:51:11 +0900

    ADD : CategoryListVC UI setup 및 tableViewDataSource 생성

--- DevSheet/DevSheet/Sources/Presentation/CategoryList/CategoryListViewController.swift
+++ DevSheet/DevSheet/Sources/Presentation/CategoryList/CategoryListViewController.swift
@@ -7,19 +7,44 @@
 
 import UIKit
 import ReactorKit
+import SnapKit
 import RxViewController
 import RxOptional
+import RxDataSources
 
 final class CategoryListViewController: BaseViewController, View {
 
     // MARK: properties
     typealias Reactor = CategoryListReactor
     var categoryGroup: MainTab?
+    private var tableViewDataSource: RxTableViewSectionedReloadDataSource<CategoryListSection>
     
     // MARK: UI
+    private let shadowView: UIView = {
+        var shadow = UIView()
+        shadow.backgroundColor = .white
+        shadow.layer.makeShadow()
+        shadow.translatesAutoresizingMaskIntoConstraints = false
+        return shadow
+    }()
+    
+    private let categoryTableView: UITableView = {
+        var tableView = UITableView()
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = .white
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(
+            CategoryListCell.self,
+            forCellReuseIdentifier: CategoryListCell.ID
+        )
+        return tableView
+    }()
+    
+    let refreshControl = UIRefreshControl()
     
     // MARK: initialize
     init(reactor: Reactor) {
+        tableViewDataSource = Self.dataSourceFactory()
         super.init(nibName: nil, bundle: nil)
         self.reactor = reactor
     }
@@ -37,7 +62,49 @@ final class CategoryListViewController: BaseViewController, View {
     // MARK: methods
     private func setupUI() {
         
+        // ViewController
+        self.view.backgroundColor = .systemGray6
+        self.navigationItem.title = self.categoryGroup?.description ?? "카테고리"
+        self.navigationController?.navigationBar.backgroundColor = .white
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.navigationController?.navigationBar.tintColor = .black
+        
+        // tablewView
+        self.view.addSubview(categoryTableView)
+        categoryTableView.snp.makeConstraints {
+            $0.width.equalToSuperview()
+            $0.top.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+            $0.centerX.equalToSuperview()
+        }
+
+        // shadow
+        self.view.addSubview(shadowView)
+        shadowView.snp.makeConstraints {
+            $0.width.equalToSuperview()
+            $0.height.equalTo(1)
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview()
+        }
+        
     }
+    
+    // MARK: Factories
+    private static func dataSourceFactory()
+    -> RxTableViewSectionedReloadDataSource<CategoryListSection> {
+        return .init { _, tableView, index, item in
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: CategoryListCell.ID,
+                for: index
+            )
+            if let categoryListCell = cell as? CategoryListCell {
+                categoryListCell.configure(with: item)
+            }
+            return cell
+        }
+    }
+    
 }
 
 // MARK: Reactor Bind

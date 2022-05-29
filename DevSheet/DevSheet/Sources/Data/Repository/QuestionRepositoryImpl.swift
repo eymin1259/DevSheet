723c1e2181d9b4bdc058e1343f3dc60ad14b37aa
eymin1259 <eymin1259@gmail.com>
2022-05-29 12:46:17 +0900

    ADD : QuestionRepositoryImpl 구현

--- DevSheet/DevSheet/Sources/Data/Repository/QuestionRepositoryImpl.swift
+++ DevSheet/DevSheet/Sources/Data/Repository/QuestionRepositoryImpl.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import RxSwift
+import Firebase
+
+final class QuestionRepositoryImpl: QuestionRepository {
+    
+    // MARK: properties
+    var firebaseService: FirebaseService
+    
+    // MARK: initialize
+    init(firebaseService: FirebaseService) {
+        self.firebaseService = firebaseService
+    }
+    
+    // MARK: methods
+    func fetchQuestions(categoryId: String) -> Single<[Question]> {
+        return firebaseService
+            .request(
+                QuestionAPI.fetchQuestions(categoryId: categoryId)
+            )
+            .map { snapshot in
+                var ret = [Question]()
+                for doc in snapshot.documents {
+                    let id = doc.documentID
+                    let data = doc.data()
+                    let question = QuestionDTO(id: id, dictionary: data).toDomain()
+                    ret.append(question)
+                }
+                return ret
+            }
+    }
+}

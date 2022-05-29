12036cef837c4f128504c63391625527cad68fa8
eymin1259 <eymin1259@gmail.com>
2022-05-29 18:38:33 +0900

    ADD : QuestionListVC TableView, TableViewCell layout 구현

--- DevSheet/DevSheet/Sources/Presentation/QuestionList/QuestionListCell.swift
+++ DevSheet/DevSheet/Sources/Presentation/QuestionList/QuestionListCell.swift
@@ -13,10 +13,30 @@ class QuestionListCell: UITableViewCell {
     // MARK: properties
     static let ID = "QuestionListCell"
     
+    // MARK: UI
+    private let questionTitleLbl: UILabel = {
+       var lbl = UILabel()
+        lbl.font = .systemFont(ofSize: 15)
+        lbl.numberOfLines = 0
+        lbl.lineBreakMode = .byWordWrapping
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        return lbl
+    }()
+    
+    private let nextBtn: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.backgroundColor = .clear
+        let img = UIImage(named: "btn_next")
+        iv.image = img
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+
     // MARK: life cycle
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
@@ -25,6 +45,26 @@ class QuestionListCell: UITableViewCell {
     
     // MARK: methods
     func setupUI() {
-      
+        // questionTitleLbl
+        contentView.addSubview(questionTitleLbl)
+        questionTitleLbl.snp.makeConstraints {
+            $0.left.equalToSuperview().inset(20)
+            $0.right.equalToSuperview().inset(46)
+            $0.top.equalToSuperview().inset(10)
+            $0.bottom.equalToSuperview().inset(10)
+        }
+        
+        // nextBtn
+        contentView.addSubview(nextBtn)
+        nextBtn.snp.makeConstraints {
+            $0.width.equalTo(6)
+            $0.height.equalTo(12)
+            $0.right.equalToSuperview().inset(20)
+            $0.centerY.equalToSuperview()
+        }
+    }
+    
+    func bind(with data: Question) {
+        questionTitleLbl.text = data.title
     }
 }
--- DevSheet/DevSheet/Sources/Presentation/QuestionList/QuestionListViewController.swift
+++ DevSheet/DevSheet/Sources/Presentation/QuestionList/QuestionListViewController.swift
@@ -17,6 +17,7 @@ final class QuestionListViewController: BaseViewController, View {
     // MARK: properties
     typealias Reactor = QuestionListReactor
     private var categoryId: String
+    private var tableViewDataSource: RxTableViewSectionedReloadDataSource<QuestionListSection>
     
     // MARK: UI
     private let questionTableView: UITableView = {
@@ -24,6 +25,8 @@ final class QuestionListViewController: BaseViewController, View {
         tableView.separatorStyle = .none
         tableView.backgroundColor = .white
         tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.rowHeight = UITableView.automaticDimension // dynamic height
+        tableView.estimatedRowHeight = 50.0
         tableView.register(
             QuestionListCell.self,
             forCellReuseIdentifier: QuestionListCell.ID
@@ -37,6 +40,7 @@ final class QuestionListViewController: BaseViewController, View {
         categoryId: String
     ) {
         self.categoryId = categoryId
+        self.tableViewDataSource = Self.tableViewDataSourceFactory()
         super.init(nibName: nil, bundle: nil)
         self.reactor = reactor
     }
@@ -53,23 +57,64 @@ final class QuestionListViewController: BaseViewController, View {
     
     // MARK: methods
     private func setupUI() {
+        // viewcontroller
+        self.view.backgroundColor = .white
+
+        // shadow
+        self.view.addSubview(shadowView)
+        shadowView.snp.makeConstraints {
+            $0.width.equalToSuperview()
+            $0.height.equalTo(1)
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview()
+        }
         
+        // tablewView
+        self.view.addSubview(questionTableView)
+        questionTableView.snp.makeConstraints {
+            $0.width.equalToSuperview()
+            $0.top.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+            $0.centerX.equalToSuperview()
+        }
+    }
+    
+    // MARK: Factories
+    private static func tableViewDataSourceFactory()
+    -> RxTableViewSectionedReloadDataSource<QuestionListSection> {
+        return .init { _, tableView, index, item in
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: QuestionListCell.ID,
+                for: index
+            )
+            if let questionListCell = cell as? QuestionListCell {
+                questionListCell.bind(with: item)
+            }
+            return cell
+        }
     }
 }
 
 // MARK: Reactor Bind
 extension QuestionListViewController {
-    
     func bind(reactor: QuestionListReactor) {
         bindAction(reactor: reactor)
         bindState(reactor: reactor)
     }
     
     private func bindAction(reactor: QuestionListReactor) {
-        
+        self.rx.viewDidAppear
+            .map { [unowned self] _ in
+                Reactor.Action.viewDidAppear(self.categoryId)
+            }
+            .bind(to: reactor.action)
+            .disposed(by: self.disposeBag)
     }
     
     private func bindState(reactor: QuestionListReactor) {
-        
+        reactor.state
+            .map { $0.questionSections }
+            .bind(to: questionTableView.rx.items(dataSource: tableViewDataSource))
+            .disposed(by: disposeBag)        
     }
 }

cd44de4317acbc2f720978a88253b3794f8f1852
eymin1259 <eymin1259@gmail.com>
2022-05-29 13:16:21 +0900

    ADD: Presentation layer QuestionListVC, QuestionListCell 기본 프로퍼티, 메소드 정의

--- DevSheet/DevSheet/Sources/Presentation/QuestionList/QuestionListCell.swift
+++ DevSheet/DevSheet/Sources/Presentation/QuestionList/QuestionListCell.swift
@@ -5,4 +5,26 @@
 //  Created by yongmin lee on 5/29/22.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class QuestionListCell: UITableViewCell {
+    
+    // MARK: properties
+    static let ID = "QuestionListCell"
+    
+    // MARK: life cycle
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: methods
+    func setupUI() {
+      
+    }
+}
--- DevSheet/DevSheet/Sources/Presentation/QuestionList/QuestionListViewController.swift
+++ DevSheet/DevSheet/Sources/Presentation/QuestionList/QuestionListViewController.swift
@@ -6,3 +6,70 @@
 //
 
 import Foundation
+import ReactorKit
+import SnapKit
+import RxViewController
+import RxOptional
+import RxDataSources
+
+final class QuestionListViewController: BaseViewController, View {    
+    
+    // MARK: properties
+    typealias Reactor = QuestionListReactor
+    private var categoryId: String
+    
+    // MARK: UI
+    private let questionTableView: UITableView = {
+        var tableView = UITableView()
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = .white
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(
+            QuestionListCell.self,
+            forCellReuseIdentifier: QuestionListCell.ID
+        )
+        return tableView
+    }()
+    
+    // MARK: initialize
+    init(
+        reactor: Reactor,
+        categoryId: String
+    ) {
+        self.categoryId = categoryId
+        super.init(nibName: nil, bundle: nil)
+        self.reactor = reactor
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    // MARK: methods
+    private func setupUI() {
+        
+    }
+}
+
+// MARK: Reactor Bind
+extension QuestionListViewController {
+    
+    func bind(reactor: QuestionListReactor) {
+        bindAction(reactor: reactor)
+        bindState(reactor: reactor)
+    }
+    
+    private func bindAction(reactor: QuestionListReactor) {
+        
+    }
+    
+    private func bindState(reactor: QuestionListReactor) {
+        
+    }
+}

65036b3fcef6b0b37c040967b45e3da6209aebb8
eymin1259 <eymin1259@gmail.com>
2022-05-29 12:41:58 +0900

    ADD : Data Layer QuestionAPI 구현

--- DevSheet/DevSheet/Sources/Data/Firebase/API/QuestionAPI.swift
+++ DevSheet/DevSheet/Sources/Data/Firebase/API/QuestionAPI.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import Firebase
+import RxSwift
+
+enum QuestionAPI {
+    case fetchQuestions(categoryId: String)
+}
+
+extension QuestionAPI: ServiceAPI {
+    
+    var collection: CollectionReference {
+        return Firestore.firestore().collection("questions")
+    }
+    
+    func task() -> Single<QuerySnapshot> {
+        switch self {
+        case .fetchQuestions(let categoryId):
+            return Single<QuerySnapshot>.create { single in
+                collection
+                    .whereField("categoryId", isEqualTo: categoryId)
+                    .getDocuments { snapshot, err in
+                        if let err = err {
+                            single(.failure(err))
+                        }
+                        if let  snapshot = snapshot,
+                           snapshot.isEmpty == false {
+                            single(.success(snapshot))
+                        } else {
+                            single(.failure(FirebaseError.noData))
+                        }
+                    }
+                return Disposables.create()
+            }
+        }
+    }
+}

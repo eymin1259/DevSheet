8a04062891deb7fdde0988756cabc437a9a5a6dc
eymin1259 <eymin1259@gmail.com>
2022-05-29 13:05:10 +0900

    ADD : QuestionListReactor 생성및 Action, Mutaion, State 정의

--- DevSheet/DevSheet/Sources/Presentation/QuestionList/QuestionListReactor.swift
+++ DevSheet/DevSheet/Sources/Presentation/QuestionList/QuestionListReactor.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import ReactorKit
+
+final class QuestionListReactor: Reactor {
+
+    // MARK: properties
+    enum Action {
+        case viewDidAppear(String) // categoryId: String
+        case refresh
+    }
+    
+    enum Mutation {
+        case setCategories([Category])
+        case setLoading(Bool)
+    }
+    
+    struct State {
+        var questionSections: [QuestionListSection] = [.init(questionList: [])]
+        var isLoading: Bool = false
+    }
+    
+    let initialState: State = .init()
+    var questionUseCase: QuestionUseCase
+    
+    // MARK: initialize
+    init(questionUseCase: QuestionUseCase) {
+        self.questionUseCase = questionUseCase
+    }
+}
+
+extension QuestionListReactor {
+    // MARK: Mutate
+    func mutate(action: Action) -> Observable<Mutation> {
+        
+        return .empty()
+    }
+    
+    // MARK: Reduce
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+ 
+        return newState
+    }
+}

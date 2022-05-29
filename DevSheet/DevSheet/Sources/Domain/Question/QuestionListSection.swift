275f2b7679ffa6a556bbd5a276929834d5cb2d9c
eymin1259 <eymin1259@gmail.com>
2022-05-29 12:54:36 +0900

    ADD : QuestoinListSection Model 구현

--- DevSheet/DevSheet/Sources/Domain/Question/QuestionListSection.swift
+++ DevSheet/DevSheet/Sources/Domain/Question/QuestionListSection.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import RxDataSources
+
+struct QuestionListSection {
+    var questionList: [Question]
+}
+
+extension QuestionListSection: SectionModelType {
+    
+    var items: [Question] {
+        return self.questionList
+    }
+    
+    init(original: QuestionListSection, items: [Question]) {
+        self = original
+        self.questionList = items
+    }
+}

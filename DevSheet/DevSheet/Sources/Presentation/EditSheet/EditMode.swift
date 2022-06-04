913265db5eca230c5b1e5aa794251b4f86253bb6
eymin1259 <eymin1259@gmail.com>
2022-06-04 14:35:50 +0900

    ADD : Edit Mode 정의 -> 족보추가, 족보수정

---
+++ DevSheet/DevSheet/Sources/Presentation/EditSheet/EditMode.swift
@@ -0,0 +1,36 @@
+//
+//  EditMode.swift
+//  DevSheet
+//
+//  Created by yongmin lee on 6/4/22.
+//
+
+import Foundation
+
+enum EditMode {
+    case ADD
+    case UPDATE
+}
+
+extension EditMode {
+    var navigationTitleText: String {
+        switch self {
+        case .ADD: return "족보 쓰기"
+        case .UPDATE: return "족보 수정"
+        }
+    }
+    
+    var defaultQuestoin: String {
+        switch self {
+        case .ADD: return "질문을 작성해주세요"
+        case .UPDATE: return "질문이 아직 작성되지 않았습니다"
+        }
+    }
+    
+    var defaultAnswer: String {
+        switch self {
+        case .ADD: return "답변을 작성해주세요"
+        case .UPDATE: return "답변이 아직 작성되지 않았습니다"
+        }
+    }
+}

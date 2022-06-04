161c51f6b7374ebe385f5c6bcb56129eb0a96844
eymin1259 <eymin1259@gmail.com>
2022-06-04 12:56:38 +0900

    ADD : AddSheetVC UI 구현

--- DevSheet/DevSheet/Sources/Presentation/AddSheet/AddSheetViewController.swift
+++ DevSheet/DevSheet/Sources/Presentation/AddSheet/AddSheetViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 import ReactorKit
 import SnapKit
+import RxCocoa
 
 final class AddSheetViewController: BaseViewController, View {
     
     // MARK: properties
     typealias Reactor = AddSheetReactor
     private var category: Category
+    let questionPlaceHolder = "질문을 작성해주세요"
+    let answerPlaceHolder = "답변을 작성해주세요"
+    
+    // MARK: UI
+    private let closeBtn: UIButton = {
+        var btn = UIButton(type: .custom)
+        btn.frame = CGRect(
+            origin: .zero,
+            size: .init(width: 30, height: 30)
+        )
+        btn.setImage(UIImage(named: "btn_close"), for: .normal)
+        return btn
+    }()
+    
+    private let saveBtn: UIButton = {
+        var btn = UIButton()
+        btn.frame = CGRect(
+            origin: .zero,
+            size: .init(width: 30, height: 30)
+        )
+        btn.setTitle("완료", for: .normal)
+        btn.setTitleColor(.orange, for: .normal)
+        btn.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        return btn
+    }()
     
     // MARK: initialize
     init(
@@ -33,12 +59,29 @@ final class AddSheetViewController: BaseViewController, View {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        textFieldDelegate()
     }
     
     // MARK: methods
     private func setupUI() {
-        view.backgroundColor = .blue
-        print("debug : AddSheetViewController setupUI category -> \(self.category)")
+        // viewcontroller
+        self.view.backgroundColor = .white
+        self.navigationItem.title = "족보 쓰기"
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: closeBtn)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: saveBtn)
+        self.addNavigationLineView()
+        // questionTitle
+        self.questionTitleTextView.text = questionPlaceHolder
+        self.questionTitleTextView.textColor = .placeholderText
+        self.questionTitleTextView.isEditable = true
+        self.addQuestionTitleTextView()
+        // TitleContentdivider
+        self.addTitleContentdividerView()
+        // AnswerContent
+        self.answerContentTextView.text = answerPlaceHolder
+        self.answerContentTextView.textColor = .placeholderText
+        self.answerContentTextView.isEditable = true
+        self.addAnswerContentTextView()
     }
 }
 
@@ -51,9 +94,53 @@ extension AddSheetViewController {
     
     private func bindAction(reactor: AddSheetReactor) {
         
+        closeBtn.rx.tap
+            .subscribe { [weak self] _ in
+                self?.dismiss(animated: true, completion: nil)
+            }.disposed(by: self.disposeBag)
+        
+        saveBtn.rx.tap
+            .subscribe { [unowned self] _ in
+            
+            }.disposed(by: self.disposeBag)
     }
     
     private func bindState(reactor: AddSheetReactor) {
         
+    }
+}
+
+// MARK: TextField Delegate
+extension AddSheetViewController {
+    func textFieldDelegate() {
+        questionTitleTextView.rx.didBeginEditing
+            .subscribe { [unowned self] _ in
+                guard questionTitleTextView.textColor == .placeholderText else { return }
+                questionTitleTextView.textColor = .label
+                questionTitleTextView.text = nil
+            }.disposed(by: self.disposeBag)
+        
+        questionTitleTextView.rx.didEndEditing
+            .subscribe { [unowned self] _ in
+                if questionTitleTextView.text.isEmpty {
+                    questionTitleTextView.text = questionPlaceHolder
+                    questionTitleTextView.textColor = .placeholderText
+                }
+            }.disposed(by: self.disposeBag)
+        
+        answerContentTextView.rx.didBeginEditing
+            .subscribe { [unowned self] _ in
+                guard answerContentTextView.textColor == .placeholderText else { return }
+                answerContentTextView.textColor = .darkGray
+                answerContentTextView.text = nil
+            }.disposed(by: self.disposeBag)
+        
+        answerContentTextView.rx.didEndEditing
+            .subscribe { [unowned self] _ in
+                if answerContentTextView.text.isEmpty {
+                    answerContentTextView.text = answerPlaceHolder
+                    answerContentTextView.textColor = .placeholderText
+                }
+            }.disposed(by: self.disposeBag)
     }
 }

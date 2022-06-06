df1a72a1f3b98cf232ec660914b1793196395bc4
eymin1259 <eymin1259@gmail.com>
2022-06-06 19:39:03 +0900

    ADD : CategoryUseCase 즐겨찾기등록 구현

--- DevSheet/DevSheet/Sources/Domain/Category/CategoryUseCase.swift
+++ DevSheet/DevSheet/Sources/Domain/Category/CategoryUseCase.swift
@@ -10,6 +10,7 @@ import RxSwift
 
 protocol CategoryUseCase {
     func fetchCategories(group: MainTab) -> Single<[Category]>
+    func saveFavoriteCategory(category: Category) -> Single<Bool>
 }
 
 final class  CategoryUseCaseImpl: CategoryUseCase {
@@ -25,5 +26,16 @@ final class  CategoryUseCaseImpl: CategoryUseCase {
     // MARK: methods
     func fetchCategories(group: MainTab) -> Single<[Category]> {
         return categoryRepository.fetchCategories(group: group)
+    }
+    
+    func saveFavoriteCategory(category: Category) -> Single<Bool> {
+        let favoriteCategories = categoryRepository.fetchFavoriteCategories().filter { favCategory in
+            favCategory.id == category.id
+        }
+        if favoriteCategories.isEmpty {
+            return categoryRepository.saveFavoriteCategory(category: category)
+        } else {
+            return Single<Bool>.just(true)
+        }
     }
 }
